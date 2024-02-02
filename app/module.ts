5eb2d77c65db7abd41d35e3b3b0222f3661a4545
Yevhen M <yevhen.melnychenko@gmail.com>
2024-02-02 22:37:32 +0200

    Initial Commit - Made structure simple, app for testing app with less structure complexity - Added common module to test package from git dependency

---
+++ app/module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
